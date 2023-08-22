b40f325f5492064933e2f7be9003b88c8dc09897
FarhanZizz <iamfarhanaziz@gmail.com>
2023-08-23 00:43:34 +0600

    frontend complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,34 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#673ab7",
+
+          secondary: "#f000b8",
+
+          accent: "#1dcdbc",
+
+          neutral: "#2b3440",
+
+          "base-100": "#f0ebf8",
+
+          info: "#1a73e8",
+
+          success: "#34a853",
+
+          warning: "#fbbd23",
+
+          error: "#d93025",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 };
